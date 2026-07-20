eee548419ca90fc9a61732c63b351347a85d46cb
duchuy605 <0306231378@caothang.edu.vn>
2026-07-20 13:46:00 +0700

    Merge branch 'hai' of https://github.com/duchuy605/do-an-tot-nghiep into dev-huy

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       navigatorKey: SocketService.navigatorKey,
-      title: 'bTaskee Helper',
+      title: 'CleanGo App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
